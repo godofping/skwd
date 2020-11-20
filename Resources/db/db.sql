94620f8771397457c53e83ebd326a125849f861f
Rex Louis <rexmicrosoft@yahoo.com.ph>
2020-11-20 13:40:22 +0800

    Update db.sql

--- Resources/db/db.sql
+++ Resources/db/db.sql
@@ -9,31 +9,32 @@ MySQL - 5.5.5-10.1.37-MariaDB : Database - vincedb
 /*!40101 SET SQL_MODE=''*/;
 
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`vincedb` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `vincedb`;
 
-/*Table structure for table `areas` */
+/*Table structure for table `area` */
 
-DROP TABLE IF EXISTS `areas`;
+DROP TABLE IF EXISTS `area`;
 
-CREATE TABLE `areas` (
+CREATE TABLE `area` (
   `areaid` int(6) NOT NULL AUTO_INCREMENT,
   `areaname` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`areaid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `areas` */
+/*Data for the table `area` */
 
-insert  into `areas`(`areaid`,`areaname`) values (1,'Tacurong City'),(2,'Isulan'),(3,'Surallah'),(5,'General Santos City');
+insert  into `area`(`areaid`,`areaname`) values (1,'Tacurong City'),(2,'Isulan'),(3,'Surallah'),(5,'General Santos City');
 
-/*Table structure for table `monthly_production_datas` */
+/*Table structure for table `monthly_production_data` */
 
-DROP TABLE IF EXISTS `monthly_production_datas`;
+DROP TABLE IF EXISTS `monthly_production_data`;
 
-CREATE TABLE `monthly_production_datas` (
+CREATE TABLE `monthly_production_data` (
   `monthlyproductiondataid` int(6) NOT NULL AUTO_INCREMENT,
   `userid` int(6) DEFAULT NULL,
   `capacity` int(11) DEFAULT NULL,
@@ -70,13 +71,13 @@ CREATE TABLE `monthly_production_datas` (
   KEY `FK_user_data_table` (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `monthly_production_datas` */
+/*Data for the table `monthly_production_data` */
 
-/*Table structure for table `pumping_stations` */
+/*Table structure for table `pumping_station` */
 
-DROP TABLE IF EXISTS `pumping_stations`;
+DROP TABLE IF EXISTS `pumping_station`;
 
-CREATE TABLE `pumping_stations` (
+CREATE TABLE `pumping_station` (
   `pumpid` int(10) NOT NULL AUTO_INCREMENT,
   `areaid` int(10) DEFAULT NULL,
   `pumpname` varchar(50) DEFAULT NULL,
@@ -84,39 +85,40 @@ CREATE TABLE `pumping_stations` (
   KEY `FK_pumping_table` (`areaid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `pumping_stations` */
+/*Data for the table `pumping_station` */
 
-insert  into `pumping_stations`(`pumpid`,`areaid`,`pumpname`) values (5,1,'Pumping Station 1'),(9,1,'Pumping Station 2'),(12,3,'Pumping Station 3');
+insert  into `pumping_station`(`pumpid`,`areaid`,`pumpname`) values (5,1,'Pumping Station 1'),(9,1,'Pumping Station 2'),(12,3,'Pumping Station 3');
 
-/*Table structure for table `users` */
+/*Table structure for table `pumpstationuser` */
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `pumpstationuser`;
 
-CREATE TABLE `users` (
+CREATE TABLE `pumpstationuser` (
+  `pumpstationuserid` int(6) DEFAULT NULL,
+  `userid` int(6) DEFAULT NULL,
+  `pumpid` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `pumpstationuser` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
   `userid` int(6) NOT NULL AUTO_INCREMENT,
   `username` varchar(60) DEFAULT NULL,
   `password` varchar(60) DEFAULT NULL,
   `fullname` varchar(60) DEFAULT NULL,
   `usertype` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `users` */
+/*Data for the table `user` */
 
-insert  into `users`(`userid`,`username`,`password`,`fullname`,`usertype`) values (3,'test','test','test test test',NULL),(14,'aw','aw','aw aw aw',NULL),(18,'vince','vince','vince ancheta',NULL);
-
-/*Table structure for table `users_assigned_areas` */
-
-DROP TABLE IF EXISTS `users_assigned_areas`;
-
-CREATE TABLE `users_assigned_areas` (
-  `userassignedareaid` int(6) DEFAULT NULL,
-  `userid` int(6) DEFAULT NULL,
-  `areaid` int(6) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Data for the table `users_assigned_areas` */
+insert  into `user`(`userid`,`username`,`password`,`fullname`,`usertype`) values (19,'admin','admin','admin','ADMIN'),(20,'user','user','user','USER');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
