b257c3a765c2299c5439246beb689811755d67cc
D Murphy <rexmicrosoft@yahoo.com.ph>
2020-11-26 23:56:47 +0800

    fix dashboard

--- Models/Database.php
+++ Models/Database.php
@@ -293,10 +293,8 @@ class Database {
 
     public function selectDashboard(){
         $stm = $this->pdo->prepare("SELECT 
-            (SELECT COUNT(*) FROM USER where usertype = 'USER') AS `users`,
-            (SELECT COUNT(*) FROM AREA) AS `areas`,
-            (SELECT COUNT(*) FROM pumping_station) AS `pumpstations`,
-            (SELECT COUNT(*) FROM monthly_production_data) AS `dataentries`");
+            *
+            FROM view_dashboard");
         $success = $stm->execute();
         $rows = $stm->fetch(PDO::FETCH_ASSOC);
         return ($success) ? $rows: [];
